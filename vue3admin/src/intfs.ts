b6e7e9689d24a72ac282508c8bb3ede4777b8aed
mirahs <mirahs@163.com>
2022-06-25 15:17:14 +0800

    vue3admin 完成管理员功能

--- vue3admin/src/intfs.ts
+++ vue3admin/src/intfs.ts
@@ -1,4 +1,11 @@
 export interface IUser {
+    id: number
     account: string
     type: number
+    type2: string
+    created_at: number
+    login_at: number
+    remark: string
+
+    locked: boolean
 }
