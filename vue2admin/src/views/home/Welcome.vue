c3dcb687ad35a260ec9c819fd83b4ef13c6375b4
mirahs <mirahs@163.com>
2022-06-25 11:43:30 +0800

    vue2admin 密码更新 http 调用改成 async

--- vue2admin/src/views/home/Welcome.vue
+++ vue2admin/src/views/home/Welcome.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="container">
-    你好, <span style="color: #f56c6c">{{ this.$store.state.userInfo.account }}</span>！
-    <template v-if="isFch">
-      月卡: {{ userInfo.fch_month_card }}, 年卡: {{ userInfo.fch_year_card }}
-    </template>
+    你好, <span style="color: #f56c6c">{{ userInfo.account }}</span>！
   </div>
 </template>
 
 
 <script>
-import common from "@/common";
-
 export default {
   name: "Welcome",
 
   computed: {
     userInfo() {
       return this.$store.state.userInfo;
-    },
-
-    isFch() {
-      return this.userInfo.type == common.adminUserTypeFch;
     },
   },
 };
--- vue2admin/src/views/sys/AdmUserPassword.vue
+++ vue2admin/src/views/sys/AdmUserPassword.vue
@@ -6,7 +6,7 @@
           <el-input v-model="model.account" readonly></el-input>
         </el-col>
       </el-form-item>
-      
+
       <el-form-item label="新密码" prop="password">
         <el-col :span="6">
           <el-input type="password" v-model="model.password" placeholder="请输入新密码" autocomplete="new-password"></el-input>
@@ -28,65 +28,59 @@
 
 
 <script>
-import user_service from '@/service/adm_user_service'
+import user_service from "@/service/adm_user_service";
 
 export default {
-  name: 'AdmUserPassword',
+  name: "AdmUserPassword",
 
   data() {
     return {
       id: this.$store.state.userInfo.id,
-      
+
       model: {
         account: this.$store.state.userInfo.account,
-        password: '',
-        repassword: '',
+        password: "",
+        repassword: "",
       },
 
       rules: {
         password: [
-          {required: true, message: "请输入新密码", trigger: "blur"},
-          {min: 5, message: "密码不能少于5个字符", trigger: "blur"},
+          { required: true, message: "请输入新密码", trigger: "blur" },
+          { min: 5, message: "密码不能少于5个字符", trigger: "blur" },
         ],
         repassword: [
-          {required: true, message: "请输入确认新密码", trigger: "blur"},
-          {min: 5, message: "密码不能少于5个字符", trigger: "blur"},
-          {validator: this.validatePasswordSame, trigger: "blur"},
-        ]
+          { required: true, message: "请输入确认新密码", trigger: "blur" },
+          { min: 5, message: "密码不能少于5个字符", trigger: "blur" },
+          { validator: this.validatePasswordSame, trigger: "blur" },
+        ],
       },
-    }
+    };
   },
 
   methods: {
     onSubmit(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          user_service.password({password: this.model.password})
-          .then(res => {
-            if (0 == res.code) {
-              this.$message({
-                type: 'success',
-                message: '更新成功',
-              });
-              this.model.password = ''
-              this.model.repassword = ''
-            }
-          })
-          return true
-        } else {
-          return false
-        }
+      this.$refs[formName].validate(async (valid) => {
+        if (!valid) return;
+
+        var res = await user_service.password({
+          password: this.model.password,
+        });
+        if (res.code != 0) return;
+
+        this.$message.success("密码更新成功");
+        
+        this.model.password = "";
+        this.model.repassword = "";
       });
     },
 
-
     validatePasswordSame(rule, value, callback) {
       if (value !== this.model.password) {
-        callback(new Error('密码不一致'))
+        callback(new Error("密码不一致"));
       } else {
-        callback()
+        callback();
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
